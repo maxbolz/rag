9701161137d609f7a8c65d42a4f3a0f99b226798
yurikakan <kan.yu@northeastern.edu>
2025-07-18 14:27:42 +1000

    docker docker docker

---
+++ postgres_services/postgres_controller.py
@@ -0,0 +1,25 @@
+import time
+from fastapi import FastAPI
+from postgres_services.postgres_dao import PostgresDao
+from scripts.pull_docs import pull_docs
+
+app = FastAPI()
+
+# Create controller instance
+postgres_dao = PostgresDao()
+
+@app.get("/related-articles")
+async def related_articles(query: str):
+    start_time = time.time()
+    result =  postgres_dao.related_articles(query)
+    end_time = time.time()
+    print(f"Time taken: {end_time - start_time} seconds")
+    return result
+
+@app.post("/upload-articles")
+async def upload_articles():
+    start_time = time.time()
+    result = pull_docs(10)
+    end_time = time.time()
+    print(f"Time taken: {end_time - start_time} seconds")
+    return result
