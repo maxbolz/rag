f5a3c9a4f09c1cb3f40ff373c7354266b27d9e6b
maxbolz <maxfbolz@gmail.com>
2025-07-23 13:09:06 +1000

    remove services

--- services/postgres/postgres_controller.py
+++ services/postgres/postgres_controller.py
@@ -1,27 +1,26 @@
 import time
 from fastapi import FastAPI
-from services.postgres.postgres_dao import PostgresDao
+from postgres_services.postgres_dao import PostgresDao
 from scripts.pull_docs import pull_docs
+import logging
 
 app = FastAPI()
 
 # Create controller instance
 postgres_dao = PostgresDao()
 
-
 @app.get("/related-articles")
 async def related_articles(query: str):
     start_time = time.time()
-    result = postgres_dao.related_articles(query)
+    result =  postgres_dao.related_articles(query)
     end_time = time.time()
-    print(f"Time taken: {end_time - start_time} seconds")
+    logging.info(f"GET Time taken: {end_time - start_time} seconds...you got that!")
     return result
-
 
 @app.post("/upload-articles")
 async def upload_articles():
     start_time = time.time()
     result = pull_docs(10)
     end_time = time.time()
-    print(f"Time taken: {end_time - start_time} seconds")
+    logging.info(f"POST Time taken: {end_time - start_time} seconds...you posted up!")
     return result
